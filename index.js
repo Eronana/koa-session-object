5762416cde48d504c32773b09200c3811ef647ea
Erona <erona@loli.bz>
2017-05-27 12:13:41 +0800

    fix

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const randomstring = require('randomstring');
 const sessions = {};
 
-const getSid = ()=>{
+const genSid = ()=>{
   const sid = randomstring.generate();
-  if(sessions in sessions) return getSid();
+  if(sessions in sessions) return genSid();
+  sessions[sid] = {};
   return sid;
 };
 
 module.exports = options => async (ctx,next) => {
   let sid = ctx.cookies.get(options.key);
-  if(!sid) ctx.cookies.set(options.key, sid = getSid(), options);
+  if(!sid) ctx.cookies.set(options.key, sid = genSid(), options);
   ctx.session = sessions[sid];
   await next();
 };
