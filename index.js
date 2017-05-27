73096f4d43bf287c35642b2322a7468cd1948757
Erona <erona@loli.bz>
2017-05-27 12:30:12 +0800

    default key: 'koa:sess'

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const getSid = ()=>{
 };
 
 module.exports = options => async (ctx,next) => {
-  let sid = ctx.cookies.get(options.key);
-  if(!sid) ctx.cookies.set(options.key, sid = getSid(), options);
+  const key = options.key || 'koa:sess';
+  let sid = ctx.cookies.get(key);
+  if(!sid) ctx.cookies.set(key, sid = getSid(), options);
   if(!(sessions[sid] instanceof Object)) sessions[sid] = {};
   ctx.session = sessions[sid];
   await next();
